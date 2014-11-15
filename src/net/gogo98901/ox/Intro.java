44e8bd35feb99110695a4b654bd203d5ad995f86
GOGO98901 <gogo98901@live.co.uk>
2014-11-15 13:45:51 +0000

    Updated intro.java

--- src/net/gogo98901/ox/Intro.java
+++ src/net/gogo98901/ox/Intro.java
@@ -2,6 +2,7 @@ package net.gogo98901.ox;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,18 +11,28 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Intro extends JPanel {
 	private static final long serialVersionUID = 1L;
 	public static String[] names = { "", "" };
-	private JTextField player1;
-	private JTextField player2;
+	private JTextField player1, player2;
 	private JTextArea page1TextArea;
+	private JLabel moreInfo;
+	private JPanel in1;
 
 	private String page1Text = "To play click on avalible grid squares (the ones that are not shaded, and not red or blue)\n" + //
 			"The aim is to get three in a row in the sperate grid on the right by getting three in a row in the sectors in the main grid\n" + //
 			"You take turns with your oponent and can go in the sectors your oponent sends you\n\n" + //
 			"The Reset button will keep the player names but reset the map\nThe New button go this screen and reset the map";
+
+	private String more = "http://github.com/GOGO98901/TicTacToe#tictactoe";
 
 	public Intro(int width, int height) {
 		setLayout(null);
@@ -49,7 +60,7 @@ public class Intro extends JPanel {
 		});
 		btnPlay.setBounds(width / 2 - 85, height - 60, 170, 26);
 		add(btnPlay);
-		
+
 		JButton btnMulti = new JButton("PLAY MULTIPLAYER");
 		btnMulti.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -61,7 +72,21 @@ public class Intro extends JPanel {
 		btnMulti.setBounds(width / 2 - 85, height - 90, 170, 26);
 		add(btnMulti);
 
-		JPanel in1 = new JPanel(new BorderLayout());
+		moreInfo = new JLabel("<html><b>Click <a href=\"" + more + "\">Here</a> to find out more</b></html>");
+		moreInfo.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				try {
+					Desktop.getDesktop().browse(new URI(more));
+				} catch (URISyntaxException | IOException ex) {
+					System.err.println(" INTRO] [ERROR] " + ex);
+				}
+			}
+		});
+		moreInfo.setHorizontalAlignment(SwingConstants.CENTER);
+		moreInfo.setBounds(0, height - 150, width, 26);
+		add(moreInfo);
+
+		in1 = new JPanel(new BorderLayout());
 		in1.setBounds(width / 2 - 300, 100, 600, 300);
 		page1TextArea = new JTextArea();
 		page1TextArea.setSelectionColor(getBackground());
